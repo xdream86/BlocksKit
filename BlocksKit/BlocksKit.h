de6c2928468778fa65a771818576fa5ffc0cbbea
Jun Xia <jxia@ilegendsoft.com>
2014-06-15 09:35:03 +0800

    test

--- BlocksKit/BlocksKit.h
+++ BlocksKit/BlocksKit.h
@@ -25,9 +25,19 @@
 //  THE SOFTWARE.
 //
 
-#import <BlocksKit/NSArray+BlocksKit.h>
-#import <BlocksKit/NSDictionary+BlocksKit.h>
-#import <BlocksKit/NSIndexSet+BlocksKit.h>
+#import <BlocksKit/NSArray+BlocksKit.h>//
+#import <BlocksKit/NSDictionary+BlocksKit.h>//
+#import <BlocksKit/NSIndexSet+BlocksKit.h>//
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
 #import <BlocksKit/NSInvocation+BlocksKit.h>
 #import <BlocksKit/NSMutableArray+BlocksKit.h>
 #import <BlocksKit/NSMutableDictionary+BlocksKit.h>
